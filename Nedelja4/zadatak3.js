065cd2b149c1e3856c26668107b7e202e15f43ad
Ненад Абрамовић <nenadabramovic@outlook.com>
2019-11-12 21:31:40 +0100

    Урађен домаћи

---
+++ Nedelja4/zadatak3.js
@@ -0,0 +1,41 @@
+let sastojak1 = {
+  name: 'Шећер',
+  qunatity: 0.1,
+  unitPrice: 55
+};
+
+let sastojak2 = {
+  name: 'Вода',
+  qunatity: 1,
+  unitPrice: 70
+};
+
+let sastojak3 = {
+  name: 'Маргарин',
+  qunatity: 0.2,
+  unitPrice: 270
+};
+
+let sastojak4 = {
+  name: 'Чоколада',
+  qunatity: 2,
+  unitPrice: 100
+};
+
+let sastojak5 = {
+  name: 'Ораси',
+  qunatity: 0.1,
+  unitPrice: 55
+};
+
+let sastojak6 = {
+  name: 'Кекс',
+  qunatity: 0.5,
+  unitPrice: 180
+};
+
+let sastojak7 = {
+  name: 'Млеко у праху',
+  qunatity: 0.4,
+  unitPrice: 120
+};
