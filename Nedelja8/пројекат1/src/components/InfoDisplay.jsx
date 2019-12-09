507556d0e5e2626c0b89831a1654b93a8f073d1b
Ненад Абрамовић <nenadabramovic@outlook.com>
2019-12-09 23:40:29 +0100

    Додат InfoDisplay

---
+++ Nedelja8/пројекат1/src/components/InfoDisplay.jsx
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from 'react';
+import { getCompanyInfo } from '../utilities/space-x-service';
+
+export const InfoDisplay = () => {
+  const [info, setInfo] = useState({});
+
+  useEffect(() => {
+    getCompanyInfo()
+      .then(data => { console.log(data); setInfo(data) });
+  }, []);
+
+  return (
+    <>
+      <h1>{info.name}</h1>
+      <label>Year founded: {info.founded}</label>
+      <br />
+      <label>Founder: {info.founder}</label>
+      <br />
+      <label>CEO: {info.ceo}</label>
+      <br />
+      <label>{info.summary}</label>
+    </>
+  );
+}
--- Nedelja8/пројекат1/src/layout/header.jsx
+++ Nedelja8/пројекат1/src/layout/header.jsx
@@ -1,24 +1,10 @@
-import React, { useState, useEffect } from 'react';
-import { getCompanyInfo } from '../utilities/space-x-service';
+import React from 'react';
+import { InfoDisplay } from '../components/InfoDisplay';
 
 export const Header = () => {
-    const [info, setInfo] = useState({});
-
-    useEffect(() => {
-        getCompanyInfo()
-        .then(data => { console.log(data);setInfo(data)});
-    }, []);
-
-    return (
-        <header>
-            <h1>{info.name}</h1>
-            <label>Year founded: {info.founded}</label>
-            <br />
-            <label>Founder: {info.founder}</label>
-            <br />
-            <label>CEO: {info.ceo}</label>
-            <br />
-            <label>{info.summary}</label>
-        </header>
-    );
+  return (
+    <header>
+      <InfoDisplay />
+    </header>
+  );
 }
